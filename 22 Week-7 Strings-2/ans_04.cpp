3ed14c8746a1592775fab418da6fbdaf845d481f
Shubham Shandilya <152802725+Shandilya009@users.noreply.github.com>
2026-04-23 03:52:35 +0530

    Update ans_04.cpp

--- 22 Week-7 Strings-2/ans_04.cpp
+++ 22 Week-7 Strings-2/ans_04.cpp
@@ -1,21 +1,24 @@
-#include<iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 #include<vector>
+#include<algorithm>
 #include<string>
-
-bool anagram(){
-    std::string str_1, str_2;
-    std::cout << "Enter string 1 = ";
-    std::getline(std::cin, str_1);
-    std::cout << "Enter string 2 = ";
-    std::getline(std::cin, str_2);
-
-    if (str_1.size() != str_2.size()) return false;
-    for (int i=0; i<str_1.size(); i++) {
-        if (str_1[i] != str_2[str_1.size()-1-i]) return false;
-    }
-    return true;
+using namespace std;
+int main() {
+string s,t;
+getline(cin,s);
+getline(cin,t);
+vector<int>v(26);
+for( char k:s){
+    v[k-'a']++;
 }
+for( char k:t){
+    v[k-'a']--;
+}
+for(int k:v){
+    if(k!=0){
+        return false;
+    }
+}return true;
 
-int main(){
-    (anagram())? std::cout << "Anagram" : std::cout << "Not Anagram";
 }
