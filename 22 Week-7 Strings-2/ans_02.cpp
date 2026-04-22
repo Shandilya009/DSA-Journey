bd0a58403716e6f84e747a91f1b5cf2fb9b6040d
Shubham Shandilya <152802725+Shandilya009@users.noreply.github.com>
2026-04-23 02:12:06 +0530

    Update ans_02.cpp

--- 22 Week-7 Strings-2/ans_02.cpp
+++ 22 Week-7 Strings-2/ans_02.cpp
@@ -1,31 +1,31 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
-#include <string>
-
-char findSecondLargestDigit(const std::string& str) {
-    char largest = '0';
-    char secondLargest = '0';
-
-    for (char ch : str) {
-        if (isdigit(ch)) {
-            if (ch > largest) {
-                secondLargest = largest;
-                largest = ch;
-            } else if (ch > secondLargest && ch != largest) {
-                secondLargest = ch;
-            }
-        }
-    }
-
-    return (secondLargest == '0') ? '-' : secondLargest;
-}
-
-
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
 int main() {
-    std::string str1 = "2947578";
-    std::cout << "Second largest digit in \"" << str1 << "\" is: " << findSecondLargestDigit(str1) << std::endl;
-
-    std::string str2 = "1241";
-    std::cout << "Second largest digit in \"" << str2 << "\" is: " << findSecondLargestDigit(str2) << std::endl;
-
+    string str = "11271";
+    int x= stoi(str);
+    int nn=x;
+    vector<int>v;
+    while(nn>0){
+        int d=nn%10;
+        v.push_back(d);
+        nn/=10;
+    }
+    int max=v[0];
+   for(int i=0;i<v.size();i++){
+       if(max<v[i]){
+           max=v[i];
+       }
+   }
+       int smax=v[0];
+   for(int i=0;i<v.size();i++){
+       if(smax<v[i] && v[i]<max){
+           smax=v[i];
+       }
+   }
+    cout<<smax;
     return 0;
 }
