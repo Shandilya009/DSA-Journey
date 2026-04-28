1601d4a78fb78efbd36f3caa217b8da10be8b6ab
Shubham Shandilya <152802725+Shandilya009@users.noreply.github.com>
2026-04-29 02:01:56 +0530

    Update ans_02.cpp

--- 37 Week-12 Prefix Sum/ans_02.cpp
+++ 37 Week-12 Prefix Sum/ans_02.cpp
@@ -1,12 +1,22 @@
-#include <vector>
-using namespace std;
 
-int pivotIndex(vector<int>& nums) {
-    int total = 0, left = 0;
-    for (int n : nums) total += n;
-    for (int i = 0; i < nums.size(); ++i) {
-        if (left == total - left - nums[i]) return i;
-        left += nums[i];
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v=nums;
+        for(int i=1;i<n;i++){
+            v[i]+=v[i-1];
+        }
+
+        for(int i=0;i<n;i++){
+            int l=(i==0?0:v[i-1]);
+            int r =v[n-1]-v[i];
+    if(l==r){
+        return i;
     }
-    return -1;
-}
+
+        }
+        return -1;
+        
+    }
+};
